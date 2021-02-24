3fb3fe788d2623e37d9131b756b84ad5d7a15634
Jon K <Johnakao@gmail.com>
2021-02-23 16:06:19 -0800

    Step 1 First Edit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Let's get this party started!");
+function getInput(e) {
+    e.preventDefault();
+    return $('.form').eq(1).val();
+}
+
+async function getGif() {
+    //get request from giphy
+    // const key = "6TRaAjG3ELVqf8GHUMy6s16UqqPVgam9";
+    let gif = await axios.get("http://api.giphy.com/v1/gifs/search", {params: { searchInput, key }});
+}
+
+$("form").on("submit", getInput);
